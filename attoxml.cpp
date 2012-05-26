9cb057efc416c95663b98226f10d676bf08b44ed
RW Penney <rwp@rwpenney.org.uk>
2012-05-26 20:47:28 +0100

    Added licensing under Apache-2.0 license.

--- attoxml.cpp
+++ attoxml.cpp
@@ -1,6 +1,22 @@
 /*
  *  Methods for simple XML document generator and printer
- *  (C)Copyright RW Penney 2012
+ *  RW Penney, May 2012
+ */
+
+/*
+ *   Copyright 2012 RW Penney <rwpenney@users.sourceforge.net>
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  */
 
 #include "attoxml.h"
@@ -259,10 +275,10 @@ int main(int argc, char *argv[])
     nd1 = doc.AppendChild("Beta");
     nd1->AddAttribute("ident", "d2cf30339783");
     nd2 = nd1->AppendChild("beta_0");
-    nd2->AppendText("Possibly");
+    nd2->AppendText("Some textual content");
     nd2->AddAttribute("float", 2 / 3.0f);
     nd2->AddAttribute("int", -34);
-    nd2->AddAttribute("string", std::string("bother"));
+    nd2->AddAttribute("key", std::string("value"));
     nd2->AddAttribute("att1", 4.5);
     nd2->AddAttribute("att2", 4.7);
     nd2->AddAttribute("attribute3", 1<<30);
